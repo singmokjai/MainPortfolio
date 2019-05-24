c4653793e3d409199b222644023281f4ef89b206
singmokjai <lijason001@gmail.com>
2019-05-24 17:29:20 -0400

    changed images and implemented grid system

---
+++ port.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.parallax-container').parallax();
+    $('.parallax').parallax();
+    $('.sidenav').sidenav();
+    $('.scrollspy').scrollSpy();
+        
+
+});
